fa3dc146582cba0387212856cd63bb1a2c684085
Zazarh-Monster <zanelemahlangu1305@gmail.com>
2023-02-18 02:47:07 -0800

    9-print_comb.c

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 
 /**
- * main - Prints all possible combinations of single-digit numbers.
+ * main - Prints all possible combinations of single-digit numbers
  *
  * Return: Always 0.
  */
@@ -14,7 +14,7 @@ int main(void)
 		putchar((num % 10) + '0');
 		if (num == 9)
 			continue;
-		
+
 		putchar(',');
 		putchar(' ');
 	}
